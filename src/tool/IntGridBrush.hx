9ec2c768af961aafcedc1f732602adcca4f6c6ab
Sébastien Bénard <github@deepnight.net>
2020-06-02 13:37:25 +0200

    Added per-layer intGrid selected value

--- src/tool/IntGridBrush.hx
+++ src/tool/IntGridBrush.hx
@@ -1,29 +1,41 @@
 package tool;
 
 class IntGridBrush extends Tool {
-	var curValue = 0;
+	static var SELECTED_VALUES : Map<LayerDef, Int> = new Map();
 
 	public function new() {
 		super();
 	}
 
+	function selectValue(v:Int) {
+		SELECTED_VALUES.set(
+			curLayer.def,
+			M.iclamp(v, 0, curLayer.def.intGridValues.length-1)
+		);
+	}
+	function getSelectedValue() {
+		return SELECTED_VALUES.exists(curLayer.def)
+			? SELECTED_VALUES.get(curLayer.def)
+			: 0;
+	}
+
 	override function updateToolBar() {
 		super.updateToolBar();
 
-		curValue = M.iclamp(curValue, 0, curLayer.def.intGridValues.length-1);
+		selectValue( getSelectedValue() );
 
 		var idx = 0;
 		for(c in curLayer.def.intGridValues) {
 			var e = new J("<li/>");
 			jToolBar.append(e);
 			e.addClass("color");
-			if( idx==curValue )
+			if( idx==getSelectedValue() )
 				e.addClass("active");
 
 			e.css("background-color", C.intToHex(c));
 			var curIdx = idx;
 			e.click( function(_) {
-				curValue = curIdx;
+				selectValue(curIdx);
 				updateToolBar();
 			});
 			idx++;
@@ -35,7 +47,7 @@ class IntGridBrush extends Tool {
 
 		dn.Bresenham.iterateThinLine(lastMouse.cx, lastMouse.cy, m.cx, m.cy, function(cx,cy) {
 			if( isAdding() )
-				curLayer.setIntGrid(cx, cy, curValue);
+				curLayer.setIntGrid(cx, cy, getSelectedValue());
 			else if( isRemoving() )
 				curLayer.removeIntGrid(cx, cy);
 		});
