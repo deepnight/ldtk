e7ca6d177af405dc6639a278f498711d288e039d
Sébastien Bénard <github@deepnight.net>
2020-06-11 14:13:45 +0200

    BoolInput now supports HTML Select

--- src/form/input/BoolInput.hx
+++ src/form/input/BoolInput.hx
@@ -1,18 +1,31 @@
 package form.input;
 
 class BoolInput extends form.Input<Bool> {
+	var isCheckBox : Bool;
+
 	public function new(j:js.jquery.JQuery, getter:Void->Bool, setter:Bool->Void) {
 		super(j, getter, setter);
 
-		if( !j.is("[type=checkbox]") )
-			throw "Only checkboxes are supported here";
+		if( !j.is("[type=checkbox], select") )
+			throw "Only CHECKBOXEs and SELECTs are supported here";
+
+		isCheckBox = j.is("[type=checkbox]");
 	}
 
 	override function parseInputValue() : Bool {
-		return input.prop("checked")==true;
+		if( isCheckBox )
+			return input.prop("checked")==true;
+		else {
+			N.debug(input.val());
+			var v = StringTools.trim( Std.string( input.val() ) ).toLowerCase();
+			return v=="true";
+		}
 	}
 
 	override function writeValueToInput() {
-		input.prop("checked", getter());
+		if( isCheckBox )
+			input.prop("checked", getter());
+		else
+			input.val( getter() ? "true" : "false" );
 	}
 }
