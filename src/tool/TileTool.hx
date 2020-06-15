b5264dcb9309efdcfe8c73ff21c5f9f5c6121f7c
Sébastien Bénard <github@deepnight.net>
2020-06-15 16:50:53 +0200

    Added Removal & Rectangle support to TileTool

--- src/tool/TileTool.hx
+++ src/tool/TileTool.hx
@@ -29,10 +29,35 @@ class TileTool extends Tool<TileSelection> {
 				client.ge.emit(LayerInstanceChanged);
 
 			case Remove:
+				dn.Bresenham.iterateThinLine(lastMouse.cx, lastMouse.cy, m.cx, m.cy, function(cx,cy) {
+					removeSelectedTileAt(cx, cy);
+				});
+				client.ge.emit(LayerInstanceChanged);
 
 			case Move:
 		}
 	}
+
+	override function useOnRectangle(left:Int, right:Int, top:Int, bottom:Int) {
+		super.useOnRectangle(left, right, top, bottom);
+
+		for(cx in left...right+1)
+		for(cy in top...bottom+1) {
+			switch curMode {
+				case null, PanView:
+				case Add:
+					drawSelectedTileAt(cx,cy);
+
+				case Remove:
+					removeSelectedTileAt(cx,cy);
+
+				case Move:
+			}
+		}
+
+		client.ge.emit(LayerInstanceChanged);
+	}
+
 
 	function drawSelectedTileAt(cx:Int, cy:Int) {
 		switch getSelectedValue() {
@@ -44,6 +69,30 @@ class TileTool extends Tool<TileSelection> {
 					client.curLayerInstance.setTile(cx,cy, 0); // TODO
 		}
 	}
+
+	function removeSelectedTileAt(cx:Int, cy:Int) {
+		switch getSelectedValue() {
+			case Single(tcx, tcy):
+				client.curLayerInstance.removeTile(cx,cy);
+
+			case Multiple(tiles):
+				// TODO
+		}
+	}
+
+	override function updateCursor(m:MouseCoords) {
+		super.updateCursor(m);
+
+		if( isRunning() && rectangle ) {
+			var r = Rect.fromMouseCoords(origin, m);
+			client.cursor.set( GridRect(curLayerInstance, r.left, r.top, r.wid, r.hei) );
+		}
+		else if( curLayerInstance.isValid(m.cx,m.cy) )
+			client.cursor.set( GridCell(curLayerInstance, m.cx, m.cy) );
+		else
+			client.cursor.set(None);
+	}
+
 
 	override function updatePalette() {
 		super.updatePalette();
