460e95257c54dc12c8ecb6b803b3557c38d1c5ec
Sébastien Bénard <github@deepnight.net>
2020-06-26 12:55:18 +0200

    Added Amazon-style floating palette auto-closing

--- src/ui/modal/FloatingToolPalette.hx
+++ src/ui/modal/FloatingToolPalette.hx
@@ -1,10 +1,15 @@
 package ui.modal;
 
 class FloatingToolPalette extends ui.Modal {
+	static var LEAVE_DIST_BEFORE_CLOSING = 90;
 	static var OVER_PADDING = 64;
 
 	public static var ME : Null<FloatingToolPalette>;
 	public var isPopOut = true;
+
+	var lastMouseX : Null<Float>;
+	var lastMouseY : Null<Float>;
+	var leavingElapsedDist = 0.;
 
 	public function new(t:Tool<Dynamic>, isPopOut:Bool) {
 		super();
@@ -14,13 +19,13 @@ class FloatingToolPalette extends ui.Modal {
 
 		jModalAndMask.addClass("floatingPalette");
 		jContent.append( t.createPalette() );
-		client.jDoc.off(".floatingPalette");
+		client.jDoc.off(".floatingPaletteEvent");
 
 		if( isPopOut ) {
 			jWrapper.mousedown( onWrapperMouseDown );
 			client.jDoc
-				.on("mousemove.floatingPalette", onDocMouseMove)
-				.on("mouseup.floatingPalette", onDocMouseUp);
+				.on("mousemove.floatingPaletteEvent", onDocMouseMove)
+				.on("mouseup.floatingPaletteEvent", onDocMouseUp);
 		}
 
 		// Detect middle click to close
@@ -60,13 +65,50 @@ class FloatingToolPalette extends ui.Modal {
 	}
 
 	function onDocMouseMove(ev:js.jquery.Event) {
-		var x = jWrapper.offset().left;
-		var y = jWrapper.offset().top;
-		var wid = jWrapper.outerWidth();
-		var hei = jWrapper.outerHeight();
+		if( lastMouseX!=null ) {
+			var wid = jWrapper.outerWidth();
+			var hei = jWrapper.outerHeight();
+			var modalX1 = jWrapper.offset().left;
+			var modalY1 = jWrapper.offset().top;
+			var modalX2 = modalX1 + wid;
+			var modalY2 = modalY1 + hei;
 
-		if( !isMouseDown && ( ev.pageX > x+wid+OVER_PADDING || ev.pageY < y-OVER_PADDING ) )
-			close();
+			if( isMouseDown || ev.pageX>=modalX1 && ev.pageX<=modalX2 && ev.pageY>=modalY1 && ev.pageY<=modalY2 ) {
+				// Over modal or dragging
+				leavingElapsedDist = 0;
+			}
+			else {
+				// Out of modal: try to determine if the cursor is moving away from the modal bounds
+				// 1 2
+				// 4 3
+				var angToCenter = Math.atan2((modalY1+hei*0.5)-ev.pageY, (modalX1+wid*0.5)-ev.pageX);
+				var angDeltaCorner1 = M.radSubstract( angToCenter, Math.atan2(modalY1-ev.pageY, modalX1-ev.pageX) );
+				var angDeltaCorner2 = M.radSubstract( angToCenter, Math.atan2(modalY1-ev.pageY, modalX2-ev.pageX) );
+				var angDeltaCorner3 = M.radSubstract( angToCenter, Math.atan2(modalY2-ev.pageY, modalX2-ev.pageX) );
+				var angDeltaCorner4 = M.radSubstract( angToCenter, Math.atan2(modalY2-ev.pageY, modalX1-ev.pageX) );
+				var minDelta = M.fmin( angDeltaCorner1, M.fmin(angDeltaCorner2, M.fmin( angDeltaCorner3, angDeltaCorner4)) );
+				var maxDelta = M.fmax( angDeltaCorner1, M.fmax(angDeltaCorner2, M.fmax( angDeltaCorner3, angDeltaCorner4)) );
+				minDelta-=0.25;
+				maxDelta+=0.25;
+
+				var angDeltaMouse = M.radSubstract( angToCenter, Math.atan2(ev.pageY-lastMouseY, ev.pageX-lastMouseX) );
+				var mouseDist = M.dist(lastMouseX, lastMouseY, ev.pageX, ev.pageY);
+
+				if( angDeltaMouse>=minDelta && angDeltaMouse<=maxDelta )
+					leavingElapsedDist-=mouseDist*3; // mouse is moving toward the modal
+				else
+					leavingElapsedDist+=mouseDist; // mouse is moving away
+				leavingElapsedDist = M.fmax(0, leavingElapsedDist);
+
+				// Close
+				if( leavingElapsedDist>=LEAVE_DIST_BEFORE_CLOSING )
+					close();
+			}
+			// Client.ME.debug("leaveDist = "+Std.int(leavingElapsedDist));
+		}
+
+		lastMouseX = ev.pageX;
+		lastMouseY = ev.pageY;
 	}
 
 	var isMouseDown = false;
@@ -81,7 +123,7 @@ class FloatingToolPalette extends ui.Modal {
 
 	override function onDispose() {
 		super.onDispose();
-		client.jDoc.off(".floatingPalette");
+		client.jDoc.off(".floatingPaletteEvent");
 		if( ME==this )
 			ME = null;
 	}
