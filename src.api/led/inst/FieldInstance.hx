158dc9752946e8d4ee3dde4f6983b00a6c5f4692
Sébastien Bénard <github@deepnight.net>
2020-07-22 12:00:51 +0200

    More human-friendly entity field serialization

--- src.api/led/inst/FieldInstance.hx
+++ src.api/led/inst/FieldInstance.hx
@@ -26,7 +26,7 @@ class FieldInstance {
 
 	public static function fromJson(project:Project, json:Dynamic) {
 		var o = new FieldInstance( project, JsonTools.readInt(json.defUid) );
-		o.internalValue = JsonTools.readEnum(ValueWrapper, json.internalValue, true);
+		o.internalValue = JsonTools.readEnum(ValueWrapper, json.realEditorValue, true);
 		return o;
 	}
 
@@ -34,7 +34,16 @@ class FieldInstance {
 		return {
 			__comment__: def.identifier+" ("+def.getShortDescription()+")",
 			defUid: defUid,
-			internalValue: JsonTools.writeEnum(internalValue,true),
+			realEditorValue: JsonTools.writeEnum(internalValue,true),
+
+			displayValue: untyped switch def.type { // only exported for readability purpose
+				case F_Int: getInt();
+				case F_Float: getFloat();
+				case F_String: getString();
+				case F_Bool: getBool();
+				case F_Color: getColorAsHexStr();
+				case F_Enum(enumDefUid): getEnumValue();
+			}
 		}
 	}
 
