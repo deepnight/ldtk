9dfedb6cdcf8ee89fdc6e241926c65e87811d859
Sébastien Bénard <github@deepnight.net>
2020-07-27 11:49:34 +0200

    Updated Warning dialog CSS

--- src/ui/modal/dialog/Warning.hx
+++ src/ui/modal/dialog/Warning.hx
@@ -2,7 +2,7 @@ package ui.modal.dialog;
 
 class Warning extends ui.modal.Dialog {
 	public function new(str:dn.data.GetText.LocaleString) {
-		super();
+		super("warning");
 
 		jContent.append('<h2>Warning</h2>');
 		var p = '<p>' + StringTools.replace(str,"\n","</p><p>") + '</p>';
