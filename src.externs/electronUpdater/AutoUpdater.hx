4720c9c766f6c8de5203631bd092c95d1ec698d0
Sébastien Bénard <github@deepnight.net>
2020-08-31 13:26:22 +0200

    Electron main part is now Haxe

---
+++ src.externs/electronUpdater/AutoUpdater.hx
@@ -0,0 +1,35 @@
+package electronUpdater;
+
+typedef UpdateInfo = {
+	/**
+	* The version.
+	*/
+	var version: String;
+	var files: Array<Dynamic>;
+
+	/**
+	* The release name.
+	*/
+	var ?releaseName: Null<String>;
+
+	/**
+	* The release notes. List if `updater.fullChangelog` is set to `true`, `string` otherwise.
+	*/
+	var ?releaseNotes: Null< haxe.extern.EitherType<String, Array<Dynamic>> >;
+
+	/**
+	* The release date.
+	*/
+	var releaseDate: String;
+
+	/**
+	* The [staged rollout](/auto-update#staged-rollouts) percentage, 0-100.
+	*/
+	var ?stagingPercentage: Int;
+}
+
+extern class AutoUpdater {
+	public function checkForUpdates() : Dynamic;
+	public function on(eventId:String, onEvent:UpdateInfo->Void) : Dynamic;
+	public function quitAndInstall(isSilent:Bool=false, isForceRunAfter:Bool=false) : Dynamic;
+}
---
+++ src.main/ElectronMain.hx
@@ -0,0 +1,102 @@
+import electron.main.App;
+import electron.main.IpcMain;
+import js.Node.__dirname;
+import js.Node.process;
+
+class ElectronMain {
+	// Boot
+	static function main() {
+		var hasDownloadedUpdate = false;
+		var isInstallingUpdate = false;
+
+		App.on('window-all-closed', function() {
+			electron.main.App.quit();
+		});
+
+		var mainWindow : electron.main.BrowserWindow = null;
+		App.on('ready', function() {
+			// Init window
+			mainWindow = new electron.main.BrowserWindow({
+				webPreferences: { nodeIntegration:true },
+				fullscreenable: true,
+				autoHideMenuBar: true,
+				show: false,
+				title: "L-Ed",
+			});
+			mainWindow.loadURL('file://$__dirname/app.html');
+			mainWindow.on('closed', function() {
+				mainWindow = null;
+			});
+			mainWindow.maximize();
+
+			// Window close button
+			mainWindow.on('close', function(ev) { // TODO move that after proper App init
+				if( !isInstallingUpdate ) {
+					ev.preventDefault();
+					mainWindow.webContents.send("winClose");
+				}
+			});
+		});
+
+
+		// *** Async invoke() handlers *****************************************************
+		IpcMain.handle("loadFile", function(event, options) {
+			js.html.Console.log(options);
+			var filePaths = electron.main.Dialog.showOpenDialogSync(null, options);
+			return filePaths==null ? null : filePaths[0];
+		});
+
+		IpcMain.handle("saveAs", function(event, options) {
+			var filePaths = electron.main.Dialog.showSaveDialogSync(null, options);
+			return filePaths==null ? null : filePaths;
+		});
+
+		IpcMain.handle("exit", function(event) {
+			App.exit();
+		});
+
+		IpcMain.handle("setFullScreen", function(event,args) {
+			mainWindow.setFullScreen(args);
+		});
+
+		IpcMain.handle("setWinTitle", function(event,args) {
+			mainWindow.title = args;
+		});
+
+
+		// *** Electron-Updater invoke handlers *****************************************************
+
+		var autoUpdater : electronUpdater.AutoUpdater = js.node.Require.require("electron-updater").autoUpdater;
+
+		IpcMain.handle("checkUpdate", function(event,args) {
+			js.html.Console.log("Checking for updates...");
+			autoUpdater.checkForUpdates();
+			autoUpdater.on('update-available', function(info) {
+				mainWindow.webContents.send('updateFound');
+			});
+			autoUpdater.on('update-downloaded', function(info) {
+				mainWindow.webContents.send('updateReady');
+				hasDownloadedUpdate = true;
+			});
+		});
+
+		IpcMain.handle("installUpdate", function(event) {
+			js.html.Console.log("install");
+			isInstallingUpdate = true;
+			autoUpdater.quitAndInstall();
+		});
+
+
+		// *** Sync send() handlers *****************************************************
+
+		IpcMain.on("getCwd", function(event) {
+			event.returnValue = process.cwd();
+		});
+
+		IpcMain.on("getAppDir", function(event) {
+			event.returnValue = App.getAppPath();
+		});
+
+
+	}
+}
