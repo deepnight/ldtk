d8fc201c090686ac32a0064b396c545234c4079f
Sébastien Bénard <github@deepnight.net>
2020-09-03 17:12:41 +0200

    Minor

--- src.renderer/page/Home.hx
+++ src.renderer/page/Home.hx
@@ -91,7 +91,7 @@ class Home extends Page {
 
 		// Open it
 		App.ME.openEditor(p, filePath);
-		N.msg("Loaded project: "+dn.FilePath.extractFileWithExt(filePath));
+		N.success("Loaded project: "+dn.FilePath.extractFileWithExt(filePath));
 		return true;
 	}
 
