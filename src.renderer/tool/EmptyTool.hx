6c840d6464a4877e606cff8e2310fb67e92918cb
Sébastien Bénard <github@deepnight.net>
2020-09-17 13:20:09 +0200

    Fixed empty tool cursor

--- src.renderer/tool/EmptyTool.hx
+++ src.renderer/tool/EmptyTool.hx
@@ -4,4 +4,9 @@ class EmptyTool extends Tool<Int> {
 	public function new() {
 		super();
 	}
+
+	override function onMouseMove(m:MouseCoords) {
+		super.onMouseMove(m);
+		editor.cursor.set(Forbidden);
+	}
 }
