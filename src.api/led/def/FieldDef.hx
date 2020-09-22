1d06e2f3f1b5598845767b823be746db6d9c34f2
Sébastien Bénard <github@deepnight.net>
2020-09-22 14:33:07 +0200

    Fixed default nullable for arrays of points

--- src.api/led/def/FieldDef.hx
+++ src.api/led/def/FieldDef.hx
@@ -29,7 +29,7 @@ class FieldDef {
 		editorDisplayMode = Hidden;
 		editorDisplayPos = Above;
 		identifier = "NewField"+uid;
-		canBeNull = type==F_String || type==F_Point;
+		canBeNull = type==F_String || type==F_Point && !isArray;
 		arrayMinLength = arrayMaxLength = null;
 		isArray = false;
 		min = max = null;
