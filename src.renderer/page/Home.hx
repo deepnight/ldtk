2f247bfafd8abb029c810ecb30c11dca4f1152e8
Sébastien Bénard <github@deepnight.net>
2020-09-24 10:26:37 +0200

    Home page fixes

--- src.renderer/page/Home.hx
+++ src.renderer/page/Home.hx
@@ -88,13 +88,27 @@ class Home extends Page {
 			var p = recents[i];
 			var li = new J('<li/>');
 			li.appendTo(jRecentList);
+
+			var jRemove = new J('<button class="remove dark">x</button>');
+			jRemove.attr("title",Lang.t._("Remove from history"));
+			var remIdx = i;
+			jRemove.click( function(_) {
+				recents.splice(remIdx,1);
+				updateRecents();
+			});
+			li.append( jRemove );
+
 			li.append( JsTools.makePath(trimmedPaths[i], C.fromStringLight(dn.FilePath.fromFile(trimmedPaths[i]).directory)) );
-			li.click( function(ev) loadProject(p) );
 			li.append( JsTools.makeExploreLink(p) );
+
+			li.click( function(ev) loadProject(p) );
+
 			if( !JsTools.fileExists(p) )
 				li.addClass("missing");
 			i--;
 		}
+
+		JsTools.parseComponents(jRecentList);
 	}
 
 
