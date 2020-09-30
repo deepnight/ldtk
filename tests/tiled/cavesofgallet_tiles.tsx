07fae0c0937a54535192ce946511352ad3c326d8
Sébastien Bénard <github@deepnight.net>
2020-09-30 17:48:51 +0200

    Added Tiled test files

---
+++ tests/tiled/cavesofgallet_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="cavesofgallet_tiles" tilewidth="8" tileheight="8" tilecount="96" columns="8">
+ <image source="cavesofgallet_tiles.png" width="64" height="96"/>
+</tileset>
---
+++ tests/tiled/objects.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="objects" tilewidth="8" tileheight="8" tilecount="80" columns="10">
+ <image source="objects.png" width="80" height="64"/>
+</tileset>
