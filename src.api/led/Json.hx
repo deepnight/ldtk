14d4291cc8a22dc96e44648afcee66915c254a20
Sébastien Bénard <github@deepnight.net>
2020-09-30 11:21:12 +0200

    Added XML to MD doc parser

--- src.api/led/Json.hx
+++ src.api/led/Json.hx
@@ -1,13 +1,28 @@
 package led;
 
+/** Root of the Json file **bold** **/
 typedef ProjectJson = {
+	/** File format version **/
 	var jsonVersion: String;
+
+	/** Default X pivot (0 to 1) for new entities **/
 	var defaultPivotX: Float;
+
+	/** Default Y pivot (0 to 1) for new entities **/
 	var defaultPivotY: Float;
+
+	/** Default grid size for new layers **/
 	var defaultGridSize: Int;
+
+	/** Project BG color **/
 	var bgColor: String;
+
+	@:hide
 	var nextUid: Int;
+
+	/** If TRUE, the Json is partially minified (no indentation, nor line breaks) **/
 	var minifyJson: Bool;
+
 	var defs: Dynamic; // JSON
 	var levels: Array<Dynamic>; // JSON
 }
---
+++ src.doc/XmlDocToMarkdown.hx
@@ -0,0 +1,63 @@
+enum FieldType {
+	Standard(name:String);
+	Unknown;
+}
+
+class XmlDocToMarkdown {
+	public static function run(xmlPath) {
+		Sys.println('Parsing $xmlPath...');
+		var raw = sys.io.File.getContent(xmlPath);
+		var xml = Xml.parse( raw );
+		var xml = new haxe.xml.Access(xml);
+
+		var md = [];
+
+		// Parse types
+		for(type in xml.node.haxe.elements) {
+			if( type.att.path.indexOf("led.")<0 )
+				continue;
+
+			Sys.println('Found ${type.name}: ${type.att.path}');
+			var name = type.att.path.substr(4);
+			md.push('## $name');
+			md.push( type.node.haxe_doc.innerHTML );
+
+			// Parse fields
+			for(field in type.node.a.elements) {
+				if( field.hasNode.meta && field.node.meta.hasNode.m && field.node.meta.node.m.att.n==":hide")
+					continue;
+
+				// Get type
+				var type : FieldType =
+					if( field.hasNode.x )
+						Standard(field.node.x.att.path);
+					else if( field.hasNode.c && field.node.c.att.path=="String" )
+						Standard("String");
+					else
+						Unknown;
+
+				Sys.println('  -> ${field.name}: $type');
+				md.push('### `${field.name}` : **${getTypeStr(type)}**');
+				if( field.hasNode.haxe_doc )
+					md.push('${field.node.haxe_doc.innerHTML}');
+			}
+		}
+
+		// Write markdown
+		var fp = dn.FilePath.fromFile(xmlPath);
+		fp.extension = "md";
+
+		Sys.println('Writing markdown: ${fp.full}...');
+		var fo = sys.io.File.write(fp.full, false);
+		fo.writeString(md.join("\n\n"));
+		fo.close();
+	}
+
+	static function getTypeStr(t:FieldType) {
+		return switch t {
+			case Standard(name): name;
+			case Unknown: "???";
+		}
+	}
+
+}
