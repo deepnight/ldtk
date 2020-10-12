e4ad6bc219410c55692fc919fc53c400110c698a
Sébastien Bénard <github@deepnight.net>
2020-10-12 16:33:37 +0200

    Updated world test map

---
+++ tests/ld47/Types.hx
@@ -0,0 +1,19 @@
+enum Affect {
+	Stun;
+}
+
+enum LevelMark {
+	Grab;
+	GrabLeft;
+	GrabRight;
+	PlatformEnd;
+	PlatformEndLeft;
+	PlatformEndRight;
+}
+
+enum DarkMode {
+	Stay;
+	Destroy;
+	DestroyAndHide;
+	GoOutOfGame;
+}
