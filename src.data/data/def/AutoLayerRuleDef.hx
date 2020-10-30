f4f5cf12dae364b75d23c29a71e78e91133f256b
Sébastien Bénard <github@deepnight.net>
2020-10-30 17:21:24 +0100

    Updated ruleDef with json

--- src.data/data/def/AutoLayerRuleDef.hx
+++ src.data/data/def/AutoLayerRuleDef.hx
@@ -101,7 +101,7 @@ class AutoLayerRuleDef {
 		return 'Rule#$uid(${size}x$size)';
 	}
 
-	public function toJson() {
+	public function toJson() : led.Json.AutoRuleDef {
 		tidy();
 
 		return {
@@ -127,7 +127,7 @@ class AutoLayerRuleDef {
 		}
 	}
 
-	public static function fromJson(jsonVersion:String, json:Dynamic) {
+	public static function fromJson(jsonVersion:String, json:led.Json.AutoRuleDef) {
 		var r = new AutoLayerRuleDef( json.uid, json.size );
 		r.active = JsonTools.readBool(json.active, true);
 		r.tileIds = json.tileIds;
