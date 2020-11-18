d3b487f93127473db4d9da87d84bd949e53661c7
Sébastien Bénard <github@deepnight.net>
2020-11-18 16:16:51 +0100

    Cleanup

--- src.data/data/Project.hx
+++ src.data/data/Project.hx
@@ -102,27 +102,19 @@ class Project {
 
 			case LinearHorizontal:
 				var wx = 0;
-				// var maxHei = 0;
 				for(l in levels) {
 					l.worldX = wx;
 					l.worldY = 0;
 					wx += l.pxWid + spacing;
-				// 	maxHei = dn.M.imax(maxHei, l.pxHei);
 				}
-				// for(l in levels)
-				// 	l.worldY = Std.int( maxHei*0.5 - l.pxHei*0.5 ); // center vertically
 
 			case LinearVertical:
 				var wy = 0;
-				// var maxWid = 0;
 				for(l in levels) {
 					l.worldX = 0;
 					l.worldY = wy;
 					wy += l.pxHei + spacing;
-					// maxHei = dn.M.imax(maxHei, l.pxHei);
 				}
-				// for(l in levels)
-				// 	l.worldY = Std.int( maxHei*0.5 - l.pxHei*0.5 ); // center vertically
 		}
 	}
 
