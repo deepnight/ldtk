2bea35dccd4504d04e24f037fda705237fdf16c5
Sébastien Bénard <github@deepnight.net>
2020-11-20 18:52:10 +0100

    Removed level selection useless scrolling

--- src.renderer/WorldTool.hx
+++ src.renderer/WorldTool.hx
@@ -171,8 +171,6 @@ class WorldTool extends dn.Process {
 				editor.selectLevel(clickedLevel);
 				if( clickedSameLevel )
 					editor.setWorldMode(false);
-				else
-					editor.camera.scrollTo(m.worldX,m.worldY);
 			}
 		}
 		else if( worldMode && clicked && !dragStarted && addMode )
