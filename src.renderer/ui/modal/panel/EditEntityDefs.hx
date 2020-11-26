94d0053c40a84912087b65e286570d82549fcd87
Sébastien Bénard <github@deepnight.net>
2020-11-26 16:39:03 +0100

    Better base name for enums

--- src.renderer/ui/modal/panel/EditEntityDefs.hx
+++ src.renderer/ui/modal/panel/EditEntityDefs.hx
@@ -77,7 +77,12 @@ class EditEntityDefs extends ui.modal.Panel {
 
 					case _:
 				}
-				var f = curEntity.createFieldDef(project, type, L.getFieldType(type), isArray);
+
+				var baseName = switch type {
+					case F_Enum(enumDefUid): project.defs.getEnumDef(enumDefUid).identifier;
+					case _: L.getFieldType(type);
+				}
+				var f = curEntity.createFieldDef(project, type, baseName, isArray);
 				editor.ge.emit( EntityFieldAdded(curEntity) );
 				selectField(f);
 				jFieldForm.find("input:not([readonly]):first").focus().select();
