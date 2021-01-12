d4ffc172f8a09a5afc168b96e765cac7a9faa010
Sébastien Bénard <github@deepnight.net>
2021-01-12 13:00:29 +0100

    Added smooth Roboto font to client assets

--- src/electron.renderer/Assets.hx
+++ src/electron.renderer/Assets.hx
@@ -1,13 +1,20 @@
 #if !macro
 
 class Assets {
-	public static var fontPixelOutline : h2d.Font;
+	public static var fontLight_small : h2d.Font;
+	public static var fontLight_medium : h2d.Font;
+	public static var fontLight_large : h2d.Font;
 	public static var fontPixel : h2d.Font;
+	public static var fontPixelOutline : h2d.Font;
+
 	public static var elements : dn.heaps.slib.SpriteLib;
 
 	public static function init() {
 		fontPixelOutline = hxd.Res.fonts.minecraftiaOutline.toFont();
 		fontPixel = hxd.Res.fonts.pixel_berry_xml.toFont();
+		fontLight_small = hxd.Res.fonts.robotoLight24.toFont();
+		fontLight_medium = hxd.Res.fonts.robotoLight48.toFont();
+		fontLight_large = hxd.Res.fonts.robotoLight72.toFont();
 
 		elements = dn.heaps.assets.Atlas.load("appElements.atlas");
 	}
