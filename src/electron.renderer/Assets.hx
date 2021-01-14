55a648fe723a500fcfd773e348fd10d7e89ea8d1
Sébastien Bénard <github@deepnight.net>
2021-01-14 10:11:26 +0100

    Updated robot fonts

--- src/electron.renderer/Assets.hx
+++ src/electron.renderer/Assets.hx
@@ -1,6 +1,7 @@
 #if !macro
 
 class Assets {
+	public static var fontLight_tiny : h2d.Font;
 	public static var fontLight_small : h2d.Font;
 	public static var fontLight_medium : h2d.Font;
 	public static var fontLight_large : h2d.Font;
@@ -13,10 +14,11 @@ class Assets {
 	public static function init() {
 		fontPixelOutline = hxd.Res.fonts.minecraftiaOutline.toFont();
 		fontPixel = hxd.Res.fonts.pixel_berry_xml.toFont();
-		fontLight_small = hxd.Res.fonts.robotoLight24.toFont();
-		fontLight_medium = hxd.Res.fonts.robotoLight36.toFont();
-		fontLight_large = hxd.Res.fonts.robotoLight48.toFont();
-		fontLight_xlarge = hxd.Res.fonts.robotoLight72.toFont();
+		fontLight_tiny = hxd.Res.fonts.roboto24.toFont();
+		fontLight_small = hxd.Res.fonts.roboto30.toFont();
+		fontLight_medium = hxd.Res.fonts.roboto36.toFont();
+		fontLight_large = hxd.Res.fonts.roboto48.toFont();
+		fontLight_xlarge = hxd.Res.fonts.roboto72.toFont();
 
 		elements = dn.heaps.assets.Atlas.load("appElements.atlas");
 	}
