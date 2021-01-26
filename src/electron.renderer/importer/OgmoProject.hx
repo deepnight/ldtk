8b341441efedccc06c8f4ff2ab268ca5dd9e2671
Sébastien Bénard <github@deepnight.net>
2021-01-26 11:43:34 +0100

    Ogmo tileset import

--- src/electron.renderer/importer/OgmoProject.hx
+++ src/electron.renderer/importer/OgmoProject.hx
@@ -44,13 +44,46 @@ class OgmoProject {
 			p.defaultLevelBgColor = p.bgColor;
 			p.defaultGridSize = readGrid(json.layerGridDefaultSize);
 
+			// Tilesets
+			log.general("Reading tileset defs...");
+			log.indentMore();
+			for(tilesetJson in json.tilesets) {
+				log.general("Found tileset "+tilesetJson.label+" ("+tilesetJson.path+")");
+				log.indentMore();
+				var td = p.defs.createTilesetDef();
+				td.identifier = data.Project.cleanupIdentifier(tilesetJson.label,true);
+				td.tileGridSize = readGrid({ x:tilesetJson.tileWidth, y:tilesetJson.tileHeight });
+				td.spacing = tilesetJson.tileSeparationX;
+				td.padding = tilesetJson.tileMarginX;
+				var path = fp.directory+"/"+tilesetJson.path;
+				var res = td.importAtlasImage(tilesetJson.path);
+				switch res {
+					case Ok:
+						log.fileOp("Image imported succesfully.");
+
+					case FileNotFound:
+						log.error("File not found: "+path);
+
+					case LoadingFailed(err):
+						log.error("Image loading failed: "+err);
+
+					case _:
+				}
+				log.indentLess();
+			}
+			log.clearIndent();
+
 			// Layers
 			log.general('Reading layer defs...');
+			json.layers.reverse();
+			log.indentMore();
 			for(layerJson in json.layers) {
-				log.general(' - Found layer ${layerJson.name} (${layerJson.definition})');
+				log.general('Found layer ${layerJson.name} (${layerJson.definition})');
+				log.indentMore();
 				ogmoLayers.set(layerJson.exportID, layerJson);
 
 				switch layerJson.definition {
+					// IntGrid layer def
 					case "grid":
 						var layer = p.defs.createLayerDef(IntGrid, data.Project.cleanupIdentifier(layerJson.name, true));
 						layer.gridSize = readGrid(layerJson.gridSize);
@@ -66,9 +99,25 @@ class OgmoProject {
 							}
 						}
 
-					case _: log.error('Unsupported layer type ${layerJson.definition}');
+					// Entity layer def
+					case "entity":
+						var layer = p.defs.createLayerDef(Entities, data.Project.cleanupIdentifier(layerJson.name, true));
+						layer.gridSize = readGrid(layerJson.gridSize);
+
+					// Tile layer def
+					case "tile":
+						var layer = p.defs.createLayerDef(Tiles, data.Project.cleanupIdentifier(layerJson.name, true));
+						layer.gridSize = readGrid(layerJson.gridSize);
+						if( layerJson.defaultTileset!=null ) {
+							log.debug("TODO tileset");
+						}
+
+					case _:
+						log.error('Unsupported layer type ${layerJson.definition}');
 				}
+				log.indentLess();
 			}
+			log.clearIndent();
 
 			log.general("Done.");
 			return p;
@@ -108,6 +157,7 @@ typedef OgmoJson = {
 	var layerGridDefaultSize : XY;
 
 	var layers: Array<OgmoLayerDef>;
+	var tilesets: Array<OgmoTilesetDef>;
 }
 
 typedef OgmoLayerDef = {
@@ -117,4 +167,20 @@ typedef OgmoLayerDef = {
 	var gridSize: XY;
 	var arrayMode: Int;
 	var legend: Map<Int,String>;
+	var defaultTileset: Null<String>;
+}
+
+typedef OgmoTilesetDef = {
+	var label: String;
+	var path: String;
+	var image: String;
+	var tileWidth: Int;
+	var tileHeight: Int;
+	var tileSeparationX: Int;
+	var tileSeparationY: Int;
+	var tileMarginX: Int;
+	var tileMarginY: Int;
+	// "tilesets": [
+		// {"label": "myIncaFront", "path": "atlas/Inca_extended_front_by_Kronbits.png", "image": "data:image/png;base64,
+		// "tileWidth": 16, "tileHeight": 16, "tileSeparationX": 0, "tileSeparationY": 0, "tileMarginX": 0, "tileMarginY": 0},
 }
