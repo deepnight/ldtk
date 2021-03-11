3578581db90dfca1a22047155d6f28cc3e4f0065
Sébastien Bénard <github@deepnight.net>
2021-03-11 17:31:45 +0100

    Added a check on New project creation (#418)

--- src/electron.renderer/page/Home.hx
+++ src/electron.renderer/page/Home.hx
@@ -477,6 +477,12 @@ class Home extends Page {
 			fp.extension = "ldtk";
 
 			var p = data.Project.createEmpty(fp.full);
+			if( !JsTools.fileExists(fp.full) ) {
+				N.error("Couldn't create this project file! Maybe try to check that you have the right to write files here.");
+				return;
+			}
+
+
 			var data = ui.ProjectSaving.prepareProjectSavingData(p);
 			new ui.ProjectSaving(this, p, (success)->{
 				if( success ) {
