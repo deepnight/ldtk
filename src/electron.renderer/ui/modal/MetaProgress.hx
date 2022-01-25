dc20a4d7bb8354e7e9277c1ef3d6803ebd0a652b
Sebastien Benard <github@deepnight.net>
2022-01-25 11:57:06 +0100

    Animated meta bar

--- src/electron.renderer/ui/modal/MetaProgress.hx
+++ src/electron.renderer/ui/modal/MetaProgress.hx
@@ -12,6 +12,9 @@ class MetaProgress extends ui.Modal {
 	var jBar : js.jquery.JQuery;
 	var title : String;
 
+	var curRatio = 0.;
+	var targetRatio = 0.;
+
 	private function new(title:String, ops:Int) {
 		super();
 
@@ -40,6 +43,10 @@ class MetaProgress extends ui.Modal {
 		if( exists() )
 			CUR.close();
 	}
+	public static inline function completeCurrent() {
+		if( exists() )
+			CUR.complete();
+	}
 
 	public static function start(title:String, ops:Int) {
 		closeCurrent();
@@ -54,21 +61,44 @@ class MetaProgress extends ui.Modal {
 		if( exists() ) {
 			CUR.idx+=n;
 			if( CUR.idx>=CUR.max )
-				closeCurrent();
+				CUR.complete();
 			else
 				CUR.updateBar();
 		}
 	}
 
+	public function complete() {
+		idx = max;
+		updateBar();
+		cd.setS("autoKillLock",0.2);
+	}
 
-	function updateBar() {
-		var pct = 100 * idx/max;
-		jBar.find(".bar").css({ width:pct+"%" });
+
+	function updateBar(immediate=false) {
+		targetRatio = 100 * idx/max;
+		if( immediate ) {
+			curRatio = targetRatio;
+			jBar.find(".bar").css({ width:targetRatio+"%" });
+		}
 	}
 
 	override function onDispose() {
 		super.onDispose();
 		if( CUR==this )
 			CUR = null;
+	}
+
+	override function update() {
+		super.update();
+
+		if( curRatio!=targetRatio ) {
+			curRatio += (targetRatio-curRatio) * ( idx>=max ? 0.6 : 0.3 );
+			if( M.fabs(curRatio-targetRatio)<=0.01 )
+				curRatio = targetRatio;
+			jBar.find(".bar").css({ width:curRatio+"%" });
+		}
+
+		if( idx>=max && !cd.has("autoKillLock") )
+			close();
 	}
 }
