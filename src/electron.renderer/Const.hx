b403b41a499740adc4b85889b38f58bb582c95e0
Sebastien Benard <github@deepnight.net>
2022-02-04 11:52:55 +0100

    Removed unused consts

--- src/electron.renderer/Const.hx
+++ src/electron.renderer/Const.hx
@@ -102,9 +102,6 @@ class Const {
 	public static var DP_UI = _inc++;
 	public static var DP_TOP = _inc++;
 
-	public static var DEFAULT_LEVEL_WIDTH = 512;
-	public static var DEFAULT_LEVEL_HEIGHT = 256;
-	public static var DEFAULT_GRID_SIZE = 16;
 	public static var MAX_GRID_SIZE = 1024;
 
 	public static var AUTO_LAYER_ANYTHING = 1000000;
