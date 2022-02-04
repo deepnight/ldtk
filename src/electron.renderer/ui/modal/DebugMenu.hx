7ef4a39753eb2e58d85d0b7856eb93e1d4c97880
Sebastien Benard <github@deepnight.net>
2022-02-04 16:26:34 +0100

    Updated debug menu

--- src/electron.renderer/ui/modal/DebugMenu.hx
+++ src/electron.renderer/ui/modal/DebugMenu.hx
@@ -309,18 +309,21 @@ class DebugMenu extends ui.modal.ContextMenu {
 			}
 		});
 
-		
+
 		addTitle(L.untranslated("Worlds"));
 		for(w in project.worlds)
 			add({
 				label: L.untranslated(w.toString()),
-				cb: ()->editor.selectWorld(w),
+				cb: ()->{
+					editor.selectWorld(w,true);
+					editor.setWorldMode(true);
+				},
 			});
 		add({
 			label: L.untranslated("Create another world"),
 			cb: ()->{
-				project.createWorld(true);
-				N.debug("created");
+				var w = project.createWorld(true);
+				editor.selectWorld(w,true);
 			}
 		});
 
