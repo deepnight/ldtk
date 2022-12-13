9f83ab8fbe54be1588091767c70fbf42d6519550
Sebastien Benard <github@deepnight.net>
2022-12-13 09:27:05 +0100

    Added extra UI scale values

--- src/electron.renderer/ui/modal/dialog/EditAppSettings.hx
+++ src/electron.renderer/ui/modal/dialog/EditAppSettings.hx
@@ -109,7 +109,7 @@ class EditAppSettings extends ui.modal.Dialog {
 		// App scaling
 		var jScale = jForm.find("#appScale");
 		jScale.empty();
-		for(s in [0.5, 0.75, 0.9, 1, 1.1, 1.25, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5]) {
+		for(s in [0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.1, 1.25, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5]) {
 			var jOpt = new J('<option value="$s"/>');
 			jScale.append(jOpt);
 			jOpt.text('${Std.int(s*100)}%');
