bbe9d528327a14a0c1aebbcd20713d114dd22b1f
Sipondo <tt_robroek@hotmail.com>
2023-07-18 19:10:00 +0200

    Basic Autocompletion

--- src/electron.renderer/ui/modal/dialog/TextEditor.hx
+++ src/electron.renderer/ui/modal/dialog/TextEditor.hx
@@ -4,22 +4,23 @@ package ui.modal.dialog;
 import codemirror.CodeMirror;
 
 class TextEditor extends ui.modal.Dialog {
-	public var jHeader : js.jquery.JQuery;
-	public var jTextArea : js.jquery.JQuery;
-	var cm: CodeMirror;
+	public var jHeader:js.jquery.JQuery;
+	public var jTextArea:js.jquery.JQuery;
 
-	public function new(str:String, title:String, ?desc:String, ?mode:ldtk.Json.TextLanguageMode, ?onChange:(str:String)->Void, ?onNoChange:Void->Void) {
+	var cm:CodeMirror;
+
+	public function new(str:String, title:String, ?desc:String, ?mode:ldtk.Json.TextLanguageMode, ?onChange:(str:String) -> Void, ?onNoChange:Void->Void) {
 		super("textEditor");
 
 		var anyChange = false;
-		var readOnly = onChange==null;
+		var readOnly = onChange == null;
 
 		new J('<h2>$title</h2>').appendTo(jContent);
 
 		jHeader = new J('<div class="header"/>');
 		jHeader.appendTo(jContent);
 
-		if( desc!=null ) {
+		if (desc != null) {
 			var parags = "<p>" + desc.split("\\n").join("</p><p>") + "</p>";
 			new J('<div class="help">$parags</div>').appendTo(jHeader);
 		}
@@ -28,51 +29,55 @@ class TextEditor extends ui.modal.Dialog {
 		jTextArea.appendTo(jContent);
 		jTextArea.val(str);
 
+		// Autocompletion
+		js.node.Require.require('codemirror/addon/hint/show-hint.js');
+		js.node.Require.require('codemirror/addon/hint/anyword-hint.js');
+		js.node.Require.require('codemirror/mode/javascript/javascript.js');
+
 		// Init Codemirror
-		cm = CodeMirror.fromTextArea( cast jTextArea.get(0), {
+		cm = CodeMirror.fromTextArea(cast jTextArea.get(0), {
 			mode: requireMode(mode),
 			theme: "lucario",
 			lineNumbers: true,
 			lineWrapping: true,
 			readOnly: readOnly,
 			autofocus: true,
+			extraKeys: {"F2": "autocomplete"},
 		});
-		cm.on("change", (ev)->anyChange=true );
+		cm.on("change", (ev) -> anyChange = true);
 
 		// Load extra addons
-		if( mode==LangXml ) {
+		if (mode == LangXml) {
 			js.node.Require.require('codemirror/addon/edit/closetag.js');
 			cm.setOption("autoCloseTags", true);
-		}
-		else {
+		} else {
 			js.node.Require.require('codemirror/addon/edit/closebrackets.js');
 			cm.setOption("autoCloseBrackets", true);
 		}
 
-		onCloseCb = ()->{
-			 var out = cm.getValue();
-			 if( anyChange && str!=out ) {
-				if( onChange!=null )
+		onCloseCb = () -> {
+			var out = cm.getValue();
+			if (anyChange && str != out) {
+				if (onChange != null)
 					onChange(out);
-			 }
-			 else if( onNoChange!=null )
+			} else if (onNoChange != null)
 				onNoChange();
 		}
 
 		addClose();
 
-		if( !readOnly )
-			addIconButton("delete", "red small delete", ()->{
+		if (!readOnly)
+			addIconButton("delete", "red small delete", () -> {
 				cm.setValue("");
 				close();
-			} );
+			});
 	}
 
 	public function scrollToEnd() {
 		cm.execCommand(GoDocEnd);
 	}
 
-	inline function requireMode(mode:ldtk.Json.TextLanguageMode) : Dynamic {
+	inline function requireMode(mode:ldtk.Json.TextLanguageMode):Dynamic {
 		// Get mode addon name
 		var modeId = switch mode {
 			case null: null;
@@ -89,34 +94,32 @@ class TextEditor extends ui.modal.Dialog {
 
 			case LangLog: "ttcn-cfg";
 		}
-		if( modeId==null )
+		if (modeId == null)
 			return null;
-
 
 		// Load language mode
 		js.node.Require.require('codemirror/mode/$modeId/$modeId.js');
-		var out : Dynamic = {
+		var out:Dynamic = {
 			name: modeId,
 		}
 
-		if( mode==LangJson )
+		if (mode == LangJson)
 			out.json = true;
 
 		return out;
 	}
 
-
 	/** Open an external text file and edit it **/
 	public static function editExternalFile(filePath:String) {
 		var fp = dn.FilePath.fromFile(filePath);
-		if( !NT.fileExists(fp.full) ) {
+		if (!NT.fileExists(fp.full)) {
 			N.error(L.t._("File not found."));
 			return false;
 		}
 
 		// Read file
 		var bytes = NT.readFileBytes(fp.full);
-		if( bytes==null ) {
+		if (bytes == null) {
 			N.error(L.t._("Could not read file content."));
 			return false;
 		}
@@ -131,25 +134,24 @@ class TextEditor extends ui.modal.Dialog {
 		var raw = bytes.toString();
 
 		var c = "";
-		for(i in 0...M.imin(256, bytes.length)) {
-			c = bytes.getString(i,1);
-			if( c==null || c.length==0 || c.charCodeAt(0) > 127 ) {
+		for (i in 0...M.imin(256, bytes.length)) {
+			c = bytes.getString(i, 1);
+			if (c == null || c.length == 0 || c.charCodeAt(0) > 127) {
 				N.error(L.t._("Hey, it looks like a binary file: this cannot be opened in this editor."));
 				return false;
 			}
 		}
 
-
 		// Guess display language
-		var mode : ldtk.Json.TextLanguageMode = null;
-		if( fp.extension!=null )
+		var mode:ldtk.Json.TextLanguageMode = null;
+		if (fp.extension != null)
 			mode = switch fp.extension.toLowerCase() {
 				case "txt", "cfg": null;
 				case "xml", "html", "xhtml", "jhtml", "tpl", "rss", "svg": LangXml;
 				case "js": LangJS;
 				case "hx", "hscript": LangHaxe;
 				case "py": LangPython;
-				case "rb","rhtml": LangRuby;
+				case "rb", "rhtml": LangRuby;
 				case "lua": LangLua;
 				case "md": LangMarkdown;
 				case "json", Const.FILE_EXTENSION, Const.LEVEL_EXTENSION: LangJson;
@@ -158,15 +160,10 @@ class TextEditor extends ui.modal.Dialog {
 			}
 
 		// Open editor
-		var editor = new TextEditor(
-			raw,
-			fp.fileWithExt,
-			mode,
-			(str)->{
-				NT.writeFileString(fp.full, str);
-				N.success( L.t._('File "::name::" saved.', { name:fp.fileWithExt }) );
-			}
-		);
+		var editor = new TextEditor(raw, fp.fileWithExt, mode, (str) -> {
+			NT.writeFileString(fp.full, str);
+			N.success(L.t._('File "::name::" saved.', {name: fp.fileWithExt}));
+		});
 		return true;
 	}
 
@@ -174,7 +171,8 @@ class TextEditor extends ui.modal.Dialog {
 		super.onKeyPress(keyCode);
 
 		switch keyCode {
-			case K.ESCAPE: close();
+			case K.ESCAPE:
+				close();
 			case _:
 		}
 	}
--- src/externs/codemirror/CodeMirror.hx
+++ src/externs/codemirror/CodeMirror.hx
@@ -1,5 +1,4 @@
 // Source: https://www.npmjs.com/package/codemirror
-
 package codemirror;
 
 @:enum
@@ -12,32 +11,32 @@ abstract ExecCommand(String) {
 
 @:jsRequire("codemirror")
 extern class CodeMirror {
-	public static function fromTextArea(e:js.html.TextAreaElement, config:CodeMirrorConfig) : CodeMirror;
+	public static function fromTextArea(e:js.html.TextAreaElement, config:CodeMirrorConfig):CodeMirror;
 
-	public function getValue(?lineSeparator:String) : String;
-	public function setValue(v:String) : Void;
-	public function setOption(name:String, val:Dynamic) : Void;
-	public function on(eventName:String, cb:(args:Dynamic)->Void) : Void;
+	public function getValue(?lineSeparator:String):String;
+	public function setValue(v:String):Void;
+	public function setOption(name:String, val:Dynamic):Void;
+	public function on(eventName:String, cb:(args:Dynamic) -> Void):Void;
 
 	/**
 		Possible values: selectAll, goDocStart/End, goLineStart/End ...
 		https://codemirror.net/doc/manual.html#commands
 	**/
-	public function execCommand(name:ExecCommand) : Void;
+	public function execCommand(name:ExecCommand):Void;
 }
 
-
 typedef CodeMirrorConfig = {
-	var ?mode: haxe.extern.EitherType<String,{}>; // string or object
-	var ?theme: String;
+	var ?mode:haxe.extern.EitherType<String, {}>; // string or object
+	var ?theme:String;
 
-	var ?lineWrapping: Bool;
-	var ?lineNumbers: Bool;
-	var ?indentUnit: Int;
-	var ?indentWithTabs: Bool;
-	var ?tabSize: Int;
-	var ?autofocus: Bool;
+	var ?lineWrapping:Bool;
+	var ?lineNumbers:Bool;
+	var ?indentUnit:Int;
+	var ?indentWithTabs:Bool;
+	var ?tabSize:Int;
+	var ?autofocus:Bool;
+	var ?extraKeys:Dynamic;
 
 	/** True, false or "nocursor" **/
-	var ?readOnly: haxe.extern.EitherType<Bool,String>;
+	var ?readOnly:haxe.extern.EitherType<Bool, String>;
 }
